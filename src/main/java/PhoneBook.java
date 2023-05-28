99f4efdf53db50cd023f7ce2dfd629c008dbee7b
Aleksandr <aleksandr.koshkin.2000@mail.ru>
2023-05-28 13:24:17 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
-    public static Integer add(String name , String phone){
-        return 0;
+    public static Map<String, String> book = new HashMap<>();
+    public static int contact = 0;
+
+    public static Integer add(String name, String phone) {
+        if (book.containsKey(name)) {
+            return -1;
+        } else {
+            book.put(name, phone);
+            contact++;
+            return contact;
+        }
     }
 }
