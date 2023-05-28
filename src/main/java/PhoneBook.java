5461ab6ad6ed65d79cd23b6d43e45d043730b2df
Aleksandr <aleksandr.koshkin.2000@mail.ru>
2023-05-28 13:39:09 +0300

    Stub and tests for findByNumber

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -2,10 +2,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PhoneBook {
-    public static Map<String, String> book = new HashMap<>();
-    public static int contact = 0;
+    public  Map<String, String> book = new HashMap<>();
+    public  int contact = 0;
 
-    public static Integer add(String name, String phone) {
+    public  Integer add(String name, String phone) {
         if (book.containsKey(name)) {
             return -1;
         } else {
@@ -13,5 +13,8 @@ public class PhoneBook {
             contact++;
             return contact;
         }
+    }
+    public  String findByNumber(String phone){
+        return  null;
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -2,19 +2,28 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PhoneBookTest {
-    PhoneBook phoneBook = new PhoneBook();
 
     @Test
     public void testAddPhoneBook() {
+        PhoneBook phoneBook = new PhoneBook();
         final int expected = 1;
         final int result = phoneBook.add("Павел", "954605533");
         Assertions.assertEquals(expected, result);
     }
     @Test
     public void testAddReplayPhoneBook() {
+        PhoneBook phoneBook = new PhoneBook();
         final int expected = -1;
         phoneBook.add("Павел", "555105533");
         final int result = phoneBook.add("Павел", "954605533");
+        Assertions.assertEquals(expected, result);
+    }
+    @Test
+    public void testFindByNumber() {
+        PhoneBook phoneBook = new PhoneBook();
+        String expected="Павел";
+        phoneBook.add("Павел", "555105533");
+        String result=phoneBook.findByNumber("555105533");
         Assertions.assertEquals(expected, result);
     }
 }
